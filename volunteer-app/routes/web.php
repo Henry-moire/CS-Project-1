0765351c24ce9be6e0db590e6d3327deac3f90ef
Yaxis <kiokojeff700@gmail.com>
2023-06-23 05:58:11 +0300

    changes to prevent back button after logout

--- volunteer-app/routes/web.php
+++ volunteer-app/routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => 'prevent-back-history'],function(){
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -29,3 +33,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+}); // prevent back middleware
